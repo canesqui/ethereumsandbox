66f4085d9c0bff1b15ba63347c6e6cc34c84527b
Ronaldo Canesqui <canesqui@gmail.com>
2018-01-23 00:16:28 -0200

    Adding deploy file

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'law basket melody coast will napkin maid desert unlock hard helmet tell',
+    'https://rinkeby.infura.io/nl2iHmjjvK9AQGn1Kr0t'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async() => {
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi There!']})
+        .send({from: accounts[0], gas: '1000000'});
+     
+     console.log('Contract deployed to', result.options.address); 
+        
+};
+deploy();
